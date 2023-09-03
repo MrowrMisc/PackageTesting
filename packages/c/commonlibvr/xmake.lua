cbc90eec57b624d3bc4486c0a4f5942378320726
Mrowr Purr <mrowr.purr@gmail.com>
2023-09-03 07:44:57 -0700

    vr test 3

--- packages/c/commonlibvr/xmake.lua
+++ packages/c/commonlibvr/xmake.lua
@@ -3,7 +3,8 @@ package("commonlibvr")
     set_description("A reverse engineered library for Skyrim Special Edition.")
     set_license("MIT")
 
-    add_urls("https://github.com/alandtse/CommonLibVR.git")
+    -- The silly thing uses git@github.com SSH path for openvr, why...
+    -- add_urls("https://github.com/alandtse/CommonLibVR.git")
 
     add_deps("fmt", "rsm-binary-io", "vcpkg::boost-stl-interfaces")
     add_deps("spdlog", { configs = { header_only = false, fmt_external = true } })
@@ -15,6 +16,32 @@ package("commonlibvr")
     end)
 
     on_install("windows|x64", function(package)
+        local current_dir = os.curdir()
+        --
+
+        local workdir = path.join(os.tmpdir(), "commonlibvr_clone")
+
+        -- Clone main repo manually.
+        os.vrun("git clone https://github.com/alandtse/CommonLibVR.git " .. workdir)
+
+        -- Change directory to the workdir.
+        os.cd(workdir)
+
+        -- Replace SSH paths with HTTPS in .gitmodules if they exist.
+        local gitmodules_path = ".gitmodules"
+        if os.isfile(gitmodules_path) then
+            local content = io.readfile(gitmodules_path)
+            content = content:gsub("git@github.com:", "https://github.com/")
+            io.writefile(gitmodules_path, content)
+        end
+
+        -- Update submodules with modified paths.
+        os.vrun("git submodule sync")
+        os.vrun("git submodule update --init --recursive")
+
+        --
+        os.cd(current_dir)
+
         os.cp(path.join(package:scriptdir(), "port", "xmake.lua"), "xmake.lua")
         import("package.tools.xmake").install(package, {})
 
