bc9f707dea5252e9549c73ec7493c65bf4688bdd
Mrowr Purr <mrowr.purr@gmail.com>
2023-09-04 07:14:29 -0700

    output

--- packages/s/skyrim-commonlib-ng/rules/plugin.lua
+++ packages/s/skyrim-commonlib-ng/rules/plugin.lua
@@ -117,3 +117,39 @@ rule("plugin")
         target:add("cxxflags", "/permissive-", "/Zc:alignedNew", "/Zc:__cplusplus", "/Zc:forScope", "/Zc:ternary")
         target:add("cxxflags", "cl::/Zc:externConstexpr", "cl::/Zc:hiddenFriend", "cl::/Zc:preprocessor", "cl::/Zc:referenceBinding")
     end)
+
+    after_build(function(target)
+        local config = target:extraconf("rules", "@skyrim-commonlib-ng/plugin")
+
+        local output_folders = config.output_folders or {}
+
+        if config.output_folder then
+            table.insert(output_folders, config.output_folder)
+        end
+
+        local dll = target:targetfile()
+        local pdb = dll:gsub("%.dll$", ".pdb")
+
+        for _, output_folder in ipairs(output_folders) do
+            local dll_target = path.join(output_folder, path.filename(dll))
+            local pdb_target = path.join(output_folder, path.filename(pdb))
+
+            -- Clean up previous files in the output folder
+            if os.isfile(dll_target) then
+                os.rm(dll_target)
+            end
+            if os.isfile(pdb_target) then
+                os.rm(pdb_target)
+            end
+
+            if not os.isdir(output_folder) then
+                os.mkdir(output_folder)
+            end
+
+            -- Copy new files to output fulder
+            os.cp(dll, output_folder)
+            if os.isfile(pdb) then
+                os.cp(pdb, output_folder)
+            end
+        end
+    end)
